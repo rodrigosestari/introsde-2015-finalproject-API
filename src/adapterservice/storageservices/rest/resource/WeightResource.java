fbc6fce6cbfc040c0d1f3b7c53138117f031e5ee
rodrigosestari <sestari@Rodrigos-MacBook-Air.local>
2016-01-06 22:21:32 +0100

    adapter clean

--- src/adapterservice/storageservices/rest/resource/WeightResource.java
+++ src/adapterservice/storageservices/rest/resource/WeightResource.java
@@ -4,6 +4,7 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -11,9 +12,14 @@ import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import adapterservice.humanapi.rest.HumanAPIClient;
+import adapterservice.humanapi.rest.entity.WeightEntity;
+import us.monoid.json.JSONArray;
+import us.monoid.json.JSONObject;
+
 @Stateless
 @LocalBean
-@Path("/measureTypes")
+@Path("/Weight")
 public class WeightResource {
 	// Allows to insert contextual objects into the class,
 	// e.g. ServletContext, Request, Response, UriInfo
@@ -22,36 +28,40 @@ public class WeightResource {
 	@Context
 	Request request;
 
-	
-	/**
-	 * Request #9: GET /measureTypes should return the list of measures your
-	 * model supports in the following formats:
-	 * 
-	 * @return
-	 * list of measure types
-	 */
 	@GET
+	@Path("{token}")
 	@Produces({ MediaType.TEXT_XML, MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	public Response getMeasureTypesXml() {
-
-		/*
-		MeasureDefinitionBean mdb = null;
+	public Response getHeight(@PathParam("token") String token) {
+		HumanAPIClient client;
+		String um = null;
+		Integer value = null;
+		String date = null;
+		
 		try {
-			System.out.println("Getting list of measures type...");
-			
-			mdb = new MeasureDefinitionBean();			
-			mdb.setMeasureType(MeasureDefinitionBean.getAll());
-			
-			if (null == mdb.getMeasureType() ) {
-				return Response.status(Response.Status.NOT_FOUND).build();
-			} else {
-				return Response.ok().entity(mdb).build();
-			}
+			client = new HumanAPIClient(token);
+			client.setDebug(true);		
+			 WeightEntity  weightEntity;			
+			 weightEntity = client.weightEntity();
+		    JSONArray vai = weightEntity.readings();
+		    if((null != null) &&  (vai.length() > 0)){
+		    	for (int i = 0; i < vai.length(); i++) {
+					JSONObject obj = vai.getJSONObject(i);
+					 um = obj.getString("unit");
+					 value = obj.getInt("value");
+					 date = obj.getString("createdAt");
+				}
+		    }else{
+		    	return Response.status(Response.Status.NOT_FOUND).build();
+		    }
+		    return Response.ok().build();
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 			return Response.serverError().build();
+		
 		}
-		*/
-		return null;
+			
 	}
+	
 	
 }
